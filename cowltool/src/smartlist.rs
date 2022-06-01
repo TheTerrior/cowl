c0e8e78bf1c9e0739111fe918cc1a512f7cc90bf
TheTerrior <werbird10@gmail.com>
2022-06-01 01:20:10 -0700

    Changed the stack frame idea for variables into a heap, need continued changes to SmartList

--- cowltool/src/smartlist.rs
+++ cowltool/src/smartlist.rs
@@ -8,16 +8,28 @@ pub struct SmartList<T> {
 
 impl<T> SmartList<T> {
     // constructor
-    fn new() -> SmartList<T> {
+    pub fn new() -> SmartList<T> {
         return SmartList {
             data: Vec::new(), //stores the actual data
-            open: VecDeque::new(), //stores the open gaps
+            open: VecDeque::with_capacity(32), //stores the open gaps
             cur: 0, //assuming no gaps, first new index; aka capacity
         }
     }
 
+    pub fn with_capacity(size: usize) -> SmartList<T> {
+        return SmartList {
+            data: Vec::with_capacity(size),
+            open: VecDeque::with_capacity(32),
+            cur: 0,
+        }
+    }
+
+    pub fn len(&mut self) -> usize {
+        return self.data.len() - self.open.len();
+    }
+
     // add a new element into the SmartList, returns the index that the item was inserted at
-    fn add(&mut self, item: T) -> u32 {
+    pub fn add(&mut self, item: T) -> u32 {
         let index_raw: u32 = { //assign index_raw to either the next open gap or the next space
             if self.open.len() > 0 {
                 self.open.pop_front().unwrap()
@@ -32,7 +44,7 @@ impl<T> SmartList<T> {
     }
 
     // remove an element at the given index
-    fn remove(&mut self, index: u32) -> Result<(), ()> {
+    pub fn remove(&mut self, index: u32) -> Result<(), ()> {
         let loc: usize = index.try_into().unwrap();
         if index >= self.cur { //if index out of bounds, return Err
             return Err(());
@@ -43,5 +55,15 @@ impl<T> SmartList<T> {
         self.open.push_back(index); //add the new gap into self.open, pop, and return
         self.data[loc] = None;
         return Ok(());
+    }
+
+    pub fn retrieve(&mut self, index: usize) -> Option<&mut T> {
+        if index > self.data.len() {
+            return None;
+        }
+        match &self.data[index] {
+            None => return None,
+            Some(_) => return self.data[index],
+        }
     }
 }
--- cowltool/src/types.rs
+++ cowltool/src/types.rs
@@ -7,6 +7,7 @@ boost the pointer type from i32 to i128 to allow for a much greater number of el
 */
 
 
+/*
 // holds both the stack and the heap, where call frames can be popped once the frame is out of scope
 pub struct Memory {
     call_stack: Vec<HashMap<u32, Variable>>, //stack frames of variable values
@@ -15,23 +16,21 @@ pub struct Memory {
     cur_pointer: u32, //decides what new name we can assign to a new pointer
     //heap: Vec<Variable>, //may be deprecated later
 }
+*/
+
+pub struct Memory {
+    pointer_stack: Vec<HashMap<u32, Pointer>>, //stack frames of pointers
+    heap: SmartList<Variable>, //collection of all Variables (can be referenced directly via pointer or via a complex type)
+}
 
 impl Memory {
     
     // create a new memory instance (one per interpreter)
     fn new() -> Memory {
         return Memory {
-            call_stack: Vec::with_capacity(1024),
             pointer_stack: Vec::with_capacity(1024),
-            cur_call: 0,
-            cur_pointer: 0,
+            heap: SmartList::with_capacity(1024),
         };
-    }
-
-    // returns mutable reference to top of call_stack
-    fn cur_call_stack(&mut self) -> &mut HashMap<u32, Variable> {
-        let len = self.call_stack.len();
-        return &mut self.call_stack[len];
     }
 
     // returns mutable reference to top of pointer_stack
@@ -40,53 +39,94 @@ impl Memory {
         return &mut self.pointer_stack[len];
     }
 
-    fn new_call_stack(&mut self) {
-
-    }
-
     fn new_pointer_stack(&mut self) {
         
-    }
-
-    fn pop_call_stack(&mut self) {
-
     }
 
     fn pop_pointer_stack(&mut self) {
         
     }
 
+    /* 
     // creates a blank pointer, helper function for define_pointer
-    fn declare_pointer(&mut self, pointer_id: u32, pointer_type: PointerType) {
+    fn declare_pointer(&mut self, pointer_id: u32, pointer_type: PointerType) -> &mut Pointer {
         self.cur_pointer_stack().insert(pointer_id, Pointer::new(pointer_type)); //insert a new pointer into the pointer stack
+        return self.find_pointer_local(pointer_id).unwrap();
     }
 
     // define a pointer, whether or not it exists, and point it to the given data
     fn define_pointer(&mut self, pointer_id: u32, pointer_type: PointerType, data: VarType) {
-        match self.find_var(pointer_id) {
+        /*
+        if no pointer exists, create it
+        if pointer exists, then tell its variable that this pointer has been removed
+        
+        in both cases, tie the pointer to the newly created variable
+        */
+
+        if let PointerType::Dyn = pointer_type { // the pointer_type is dyn, so all types are valid and always will be
+            //define a pointer of type dyn
+        } else {
+            //type of pointer_type and data must match, var is wildcard though
+        }
+
+        self.create_var(data)
+
+
+        match pointer_option {
             None => { //create the new pointer
-                self.declare_pointer(pointer_id, pointer_type);
+                pointer_ref = self.declare_pointer(pointer_id, pointer_type);
             }
             Some(var_ref) => { //pointer already exists
-
+                pointer_ref = var_ref;
+                self.find_var(pointer_ref.var_index.unwrap());
             }
         }
     }
+    */
 
+    // overwrites any previous pointer data if it existed, must receive an id to a Variable
+    fn declare_pointer(&mut self, pointer_id: u32, pointer_type: PointerType, var_id: u32) -> Result<(), ()>{
+        self.cur_pointer_stack().insert(pointer_id, Pointer::new(pointer_type)); //insert new pointer into the stack with the given id
+        if vartype_to_pointertype(&self.find_var(var_id).unwrap().data) != pointer_type { //ensure the types match up
+            return Err(());
+        }
+        let pointer_pointer: &mut Pointer = self.find_pointer_local(pointer_id).unwrap();
+        pointer_pointer.var_index = Some(var_id);
+        return Ok(());
+    }
+
+    /* 
     // like define, however requires the variable to already exist, modifies instead of redefines the pointer at id, may cause errors at many points
     fn assign_variable(&mut self, pointer_id: u32, data: VarType) {
         let var_id: u32 = self.cur_pointer_stack().get(&pointer_id).unwrap().data.unwrap(); //get id of the internal variable
-        self.find_var(var_id).unwrap().data = data; //modify the variable
+        self.find_pointer(var_id).unwrap().data = data; //modify the variable
     }
+    */
 
-    // if a variable with the given id exists, find it and return a reference to it
-    fn find_var(&mut self, id: u32) -> Option<&mut Variable> {
-        for i in (0..self.call_stack.len()).rev() {
-            if self.call_stack[i].contains_key(&id) {
-                return self.call_stack[i].get_mut(&id);
+    // if a pointer with the given id exists, find it and return a reference to it
+    fn find_pointer_global(&mut self, pointer_id: u32) -> Option<&mut Pointer> {
+        for i in (0..self.pointer_stack.len()).rev() {
+            if self.pointer_stack[i].contains_key(&pointer_id) {
+                return self.pointer_stack[i].get_mut(&pointer_id);
             }
         }
         return None;
+    }
+
+    fn find_pointer_local(&mut self, pointer_id: u32) -> Option<&mut Pointer> {
+        let cur = self.cur_pointer_stack();
+        if cur.contains_key(&pointer_id) {
+            return cur.get_mut(&pointer_id);
+        }
+        return None;
+    }
+
+    fn find_var(&mut self, var_id: u32) -> Option<&mut Variable> {
+        return self.heap.retrieve(var_id.try_into().unwrap());
+    }
+
+    fn create_var(&mut self, data: Variable) -> u32 {
+        return self.heap.add(data);
     }
 }
 
@@ -114,6 +154,7 @@ impl Variable {
 }
 
 // utilized by the Variable struct
+#[derive(Debug, PartialEq)]
 pub enum VarType {
     //simple types
     Bool(bool),
@@ -131,10 +172,11 @@ pub enum VarType {
     Dict(HashMap<u32,i32>),
     List(Vec<u32>),
     String(Vec<u32>),
-    Struct, //tba
+    Struct(Option<i32>), //tba
 }
 
 // utilized by the Pointer struct
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum PointerType {
     //simple types
     Bool,
@@ -151,8 +193,8 @@ pub enum PointerType {
     Array,
     Dict,
     List,
-    Struct,
     String,
+    Struct,
 
     //weak types
     Dyn,
@@ -162,22 +204,79 @@ pub enum PointerType {
 // this structure is the front-end for a variable, generally immutable, replaced rather than modified
 pub struct Pointer {
     var_type: PointerType,
-    data: Option<u32>, //simply points to a location in memory, which can either be a complex or simple variable
+    var_index: Option<u32>, //simply points to a location in memory, which can either be a complex or simple variable
 }
 
 impl Pointer {
     fn new(pointer_type: PointerType) -> Pointer {
         return Pointer {
             var_type: pointer_type,
-            data: None,
+            var_index: None,
         }
     }
 
-    fn change_location(&mut self, location: Option<u32>) {
-        self.data = location;
+    fn change_location(&mut self, var_index: Option<u32>) {
+        self.var_index = var_index;
     }
 }
 
+// i hate myself, why did i do this *facepalm*
+fn vartype_to_pointertype(var_type: &VarType) -> PointerType {
+    match var_type {
+        VarType::Bool(_) => {
+            return PointerType::Bool;
+        },
+        VarType::Char(_) => {
+            return PointerType::Char;
+        },
+        VarType::Float32(_) => {
+            return PointerType::Float32;
+        },
+        VarType::Float64(_) => {
+            return PointerType::Float64;
+        },
+        VarType::Int8(_) => {
+            return PointerType::Int8;
+        },
+        VarType::Int16(_) => {
+            return PointerType::Int16;
+        },
+        VarType::Int32(_) => {
+            return PointerType::Int32;
+        },
+        VarType::Int64(_) => {
+            return PointerType::Int64;
+        },
+        VarType::Int128(_) => {
+            return PointerType::Int128;
+        },
+        VarType::Array(_) => {
+            return PointerType::Array;
+        },
+        VarType::Dict(_) => {
+            return PointerType::Dict;
+        },
+        VarType::List(_) => {
+            return PointerType::List;
+        },
+        VarType::String(_) => {
+            return PointerType::String;
+        },
+        VarType::Struct(_) => {
+            return PointerType::Struct;
+        },
+    }
+}
+
+fn check_type_match(pointer_type: PointerType, var_type: VarType) -> bool {
+    if let PointerType::Dyn = pointer_type {
+        return true;
+    }
+    if vartype_to_pointertype(&var_type) == pointer_type {
+        return true;
+    }
+    return false;
+}
 
 /* 
 // main structure of a variable in Cowl, contains the different types a variable can be
