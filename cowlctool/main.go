6d4639660e6c4abdd92f870368d51b8322d07d69
Brendan-alt <987654321kaboommc@gmail.com>
2022-07-24 18:14:43 -0700

    string_to_bytes parsing for 1 & 0 string values

--- cowlctool/main.go
+++ cowlctool/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func check(err error) {
@@ -11,47 +12,84 @@ func check(err error) {
 	}
 }
 
-// convert the string of 1's and 0's into bytes to save into the bytecode file
+// Convert the string of 1's and 0's into bytes to save into the bytecode file
 func string_to_bytes(input string) []byte {
-	var result int = len(input) % 8
-	var ret []byte
-	if result == 0 {
-		ret = make([]byte, len(input) / 8)
+	var ret []byte                      //Return value
+	var retCrawler int = 0              //Current byte position in the return array
+	var isDangling bool = false         //Boolean to determine if values are dangling
+	var danglingAmount = len(input) % 8 //The amount of dangling values to parse
+
+	//Allocate a byte array based on the size of string, create one extra byte for any dangling values
+	if danglingAmount == 0 {
+		ret = make([]byte, len(input)/8)
 	} else {
-		ret = make([]byte, len(input) / 8 + 1)
+		ret = make([]byte, len(input)/8+1)
+		isDangling = true
 	}
 
-	/*
-	for i := 0; i < len(input); i++ {
-		if (input[i] != byte() && input[i]) != "1" {
-			fmt.Println("ERROR: Compiler received values other than 0 and 1")
-			panic("Compiler error")
-		} 
+	//Iterate through the entire length of the input string
+	for inputCrawler := 0; inputCrawler < len(input); {
+		var splicedInput string = ""
+		var amountToParse int = 8 //Tell the crawler how many bits to parse, defaulting to 8
+
+		//Only when reaching the last byte check to see if the amount to parse needs to be reduced
+		if inputCrawler/8 == len(input)/8 && isDangling {
+			//If there is a dangling value parse only that amount instead
+			///fmt.Println("Excess: ", danglingAmount)
+			amountToParse = danglingAmount
+		}
+
+		//Parse a given amount of values from the input string into a temporary splicedInput value
+		for i := 0; i < amountToParse; i++ {
+			if input[inputCrawler] != byte(0) && input[inputCrawler] != byte(1) { //Check if the current character is a 1 or 0 byte
+				///fmt.Print(input[inputCrawler], " ")
+				splicedInput += string(input[inputCrawler]) //Append the valid values to a spliced input
+			} else {
+				fmt.Println("ERROR: Compiler received values other than 0 and 1") //Otherwise throw an error and terminate the program
+				panic("Compiler error")
+			}
+			inputCrawler++
+		}
+		///fmt.Println("\nSPLICED INPUT:", splicedInput)
+
+		// if not a normal sized byte (8 bits) pad with zeros to fill remaining space
+		if amountToParse < 8 {
+			for i := 0; i < 8-amountToParse; i++ {
+				splicedInput += "0"
+			}
+			///fmt.Println("PADDED INPUT:", splicedInput)
+		}
+
+		//Convert a size 8 splicedInput into a Uint value using strconv.ParseUint
+		if i, err := strconv.ParseUint(splicedInput, 2, 8); err != nil { //strconv.ParseInt(string, base, bitsize)
+			fmt.Println(err)
+			panic("Compiler Error")
+		} else {
+			//fmt.Printf("%T, %v\n", uint8(i), uint8(i))
+			ret[retCrawler] = byte(uint8(i)) //i value is int64 by default from ParseUint
+			retCrawler++
+		}
 	}
-	*/
-
-	var bytes []byte = []byte(input) //each byte here represents a single character, either "0" or "1"
-	fmt.Println(bytes)
-
+	///fmt.Println(ret)
 	return ret
 }
 
 // see main.rs:process_arguments() for more details, these two functions overlap greatly
 func process_arguments() {
 	var args []string = os.Args
-	
+
 	//var target string //target file we'll be compiling
 	//var output string //output file of our compiled bytecode
-	
+
 	//var mode int32 = 0
 	var help bool = false
 	var version bool = false
 	//var too_many_args bool = true
 
 	/*
-        mode:
-        0 -> default; flags or code directory
-    */
+	   mode:
+	   0 -> default; flags or code directory
+	*/
 
 	for i := 0; i < len(args); i++ {
 		if i == 0 {
@@ -68,8 +106,8 @@ func process_arguments() {
 
 	// I have no idea why Go acts this weird with multiline strings
 	if help {
-        fmt.Println(
-`Cowl's compiler
+		fmt.Println(
+			`Cowl's compiler
 
 USAGE:
 	cowlc [OPTIONS] [TARGET FILE]
@@ -79,8 +117,8 @@ OPTIONS:
     	-h, --help	Print help information (this message), and exit
     	-O, --optimize	Compile into a larger bytecode file that runs faster
     	-V, --version	Print version info and exit`)
-        return
-    }
+		return
+	}
 	if version {
 		fmt.Println("cowlc 1.0.0")
 		return
@@ -92,30 +130,36 @@ func main() {
 	process_arguments()
 
 	testing()
-	
-
 }
 
 func testing() {
-	var args []string = os.Args[1:]
-	fmt.Println(os.Args[0])
-	fmt.Println(args)
+	//Testing code for ARGS
+	///var args []string = os.Args[1:]
+	///fmt.Println(os.Args[0])
+	///fmt.Println(args)
 
-	fmt.Println("wassup")
 	file, err := os.Create("../cowltool/src/test.cowlc")
 	check(err)
 	defer file.Close()
 
-	//var text string = "a"
-	//var arr []byte = []byte{115, 111, 109, 101, 10, 255}
-	var bytes_string string = "010101011"
+	var bytes_string string = "011000010110001001100011111"
 	var bytes []byte = string_to_bytes(bytes_string)
-	var thing []byte = []byte{1};
 
-	//file.WriteString(text)
-	//file.Write(arr)
-	file.Write(thing)
 	file.Write(bytes)
 	file.Sync()
-	fmt.Println(len(bytes))
+	///fmt.Println(len(bytes))
+
+	//Testing concurrent writes:
+	//var bytes_string_secondary string = "011000010110001001100011001100010011001000110011010000010100001001000011001000010100000000100011"
+	//bytes = string_to_bytes(bytes_string_secondary)
+	//file.Write(bytes)
+	//file.Sync()
+
+	/*TESTING OPTIONS:
+	01100001 = a
+	011000010110001001100011 = abc
+	011000010110001001100011001100010011001000110011010000010100001001000011001000010100000000100011 = abc123ABC!@#
+	010101011 = Initial Test String (Garbage)
+	011000010110001001100011111 = abc + [111] excess bytes
+	*/
 }
