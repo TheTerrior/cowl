019548afc4713ee9882998d2fe04c764fa9f3918
Brendan-alt <987654321kaboommc@gmail.com>
2022-08-13 18:45:39 -0700

    Insert leading 0's & remove excess bool

--- cowlctool/main.go
+++ cowlctool/main.go
@@ -16,7 +16,6 @@ func check(err error) {
 func string_to_bytes(input string) []byte {
 	var ret []byte                      //Return value
 	var retCrawler int = 0              //Current byte position in the return array
-	var isDangling bool = false         //Boolean to determine if values are dangling
 	var danglingAmount = len(input) % 8 //The amount of dangling values to parse
 
 	//Allocate a byte array based on the size of string, create one extra byte for any dangling values
@@ -24,7 +23,6 @@ func string_to_bytes(input string) []byte {
 		ret = make([]byte, len(input)/8)
 	} else {
 		ret = make([]byte, len(input)/8+1)
-		isDangling = true
 	}
 
 	//Iterate through the entire length of the input string
@@ -33,7 +31,7 @@ func string_to_bytes(input string) []byte {
 		var amountToParse int = 8 //Tell the crawler how many bits to parse, defaulting to 8
 
 		//Only when reaching the last byte check to see if the amount to parse needs to be reduced
-		if inputCrawler/8 == len(input)/8 && isDangling {
+		if inputCrawler/8 == len(input)/8 && danglingAmount != 0 {
 			//If there is a dangling value parse only that amount instead
 			///fmt.Println("Excess: ", danglingAmount)
 			amountToParse = danglingAmount
@@ -55,9 +53,9 @@ func string_to_bytes(input string) []byte {
 		// if not a normal sized byte (8 bits) pad with zeros to fill remaining space
 		if amountToParse < 8 {
 			for i := 0; i < 8-amountToParse; i++ {
-				splicedInput += "0"
+				splicedInput = "0" + splicedInput //Insert zeros to the front of the extra segment
 			}
-			///fmt.Println("PADDED INPUT:", splicedInput)
+			fmt.Println("PADDED INPUT:", splicedInput)
 		}
 
 		//Convert a size 8 splicedInput into a Uint8 value using strconv.ParseUint
@@ -141,7 +139,7 @@ func testing() {
 	check(err)
 	defer file.Close()
 
-	var bytes_string string = "011000010110001001100011"
+	var bytes_string string = "011000010110001001100011111"
 	var bytes []byte = string_to_bytes(bytes_string)
 
 	file.Write(bytes)
